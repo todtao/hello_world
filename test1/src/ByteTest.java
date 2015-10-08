42a827ce39c00fd1922a27157d12a85272750e98
tod.tao <tod.tao@vipshop.com>
2015-10-08 12:14:02 +0800

    - todtao 试试

---
+++ test1/src/ByteTest.java
@@ -0,0 +1,16 @@
+/**
+ * Created by tod.tao on 2015/8/20.
+ */
+public class ByteTest {
+    public static void main(String[] args) {
+//        byte[] a= {1,0,0,11,78};
+//        String[] b= new String[5];
+//        for(int i=0;i<a.length;i++){
+//            b[i] = Integer.toHexString(a[i]);
+//            System.out.println(b[i]);
+//        }
+        Integer a = 128;
+        Integer b = 128;
+        System.out.println(a==b);
+    }
+}
